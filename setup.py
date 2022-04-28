61da9028421131f74e9538f1d698c7f8644f8574
LupineDev <chris@realgeeks.com>
2022-04-28 10:40:13 -1000

    Bump version to 2.0.3
    Co-authored-by: Chris Cenatiempo <chrisc@realgeeks.com>
    Co-authored-by: Katie Perry <katieperry.dev@gmail.com>

--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ class PyTest(TestCommand):
 
 setup(
     name='leadrouter',
-    version='2.0.2',
+    version='2.0.3',
 
     description='Python Client to Real Geeks REST API',
     url='https://github.com/RealGeeks/lead_router.py',
